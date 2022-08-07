88459c2e83b95562f2640e61dbef1d8de10957a3
dr-ydh <okdohyuk@drdiary.co.kr>
2022-08-07 16:43:28 +0900

    [jest] 가독성을 위해 it을 사용하기도 한다

--- __tests__/sum.spec.js
+++ __tests__/sum.spec.js
@@ -1,5 +1,10 @@
 const sum = require('../sum');
 
-test('1 + 2 = 3', () => {
+it('1 + 2 의 결과는 3이다.', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+/*
+test('1 + 2 = 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});*/
