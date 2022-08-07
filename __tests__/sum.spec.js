56ce99c74472bcffe07add1f47079dca843b1f31
dr-ydh <okdohyuk@drdiary.co.kr>
2022-08-07 17:00:20 +0900

    [jest] describe를 사용하여 여러 함수를 그룹을 지어 테스트 할 수 있다.

--- __tests__/sum.spec.js
+++ __tests__/sum.spec.js
@@ -1,8 +1,19 @@
-const sum = require('../sum');
+const { sum, sumOf } = require('../sum');
 
-it('1 + 2 의 결과는 3이다.', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('sum', () => {
+    it('calculates 1 + 2', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    it('sum all numbers', () => {
+        const numbers = [1, 2, 3];
+        expect(sumOf(numbers)).toBe(6);
+    });
 });
+
+/*it('1 + 2 의 결과는 3이다.', () => {
+    expect(sum(1, 2)).toBe(3);
+});*/
 
 /*
 test('1 + 2 = 3', () => {
--- sum.js
+++ sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
     return a + b;
-}
+};
 
-module.exports = sum; // 내보내기
+function sumOf(numbers) {
+    let result = 0;
+    numbers.forEach((number) => result += number);
+    return result;
+};
+
+// 각각 내보내기
+exports.sum = sum;
+exports.sumOf = sumOf;
